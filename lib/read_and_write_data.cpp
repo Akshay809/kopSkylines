be4bc197a2596998bbb456b20572f618e437d8f8
Akshay Agrawal <akshay809@gmail.com>
2015-03-21 23:47:46 +0530

    Define the methods declared in read-and-write-data header file

---
+++ lib/read_and_write_data.cpp
@@ -0,0 +1,10 @@
+#include <read_and_write_data.h>
+using namespace std;
+
+void importData(FileReader &reader, vector<DataObject> &data) {
+	// Definition goes here!!
+}
+
+void exportData(FileWriter &writer, const vector<DataObject> &data) {
+	// Definition goes here!!
+}
