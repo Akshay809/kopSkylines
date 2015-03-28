f5ec548d6a2731e95322d67b73b3d035573f97fa
Akshay Agrawal <akshay809@gmail.com>
2015-03-29 01:05:37 +0530

    Update vectors to store pointers

--- lib/DataInstance.cpp
+++ lib/DataInstance.cpp
@@ -15,10 +15,10 @@ bool DataInstance::isMinimumCornerOfU() {
 
 bool DataInstance::isDominatedBy(DataInstance& I) {
 	DataMap dataStoreOfI = I.getDataStore();
-	DataIterator itrU = this->dataStore.begin();
+	DataIterator itrU = dataStore.begin();
 	DataIterator itrI = dataStoreOfI.begin();
-	while(itrU!=this->dataStore.end() && itrI!=dataStoreOfI.end()) {
-		if(itrU->second.compareWith(itrI->second) == -1)
+	while(itrU!=dataStore.end() && itrI!=dataStoreOfI.end()) {
+		if((itrU->second)->compareWith(*(itrI->second)) == -1)
 			return false;
 		itrU++; itrI++;
 	}
@@ -27,20 +27,28 @@ bool DataInstance::isDominatedBy(DataInstance& I) {
 
 void DataInstance::minimizeWRT(DataInstance& I) {
 	DataMap dataStoreOfI = I.getDataStore();
-	DataIterator itrU = this->dataStore.begin();
+	DataIterator itrU = dataStore.begin();
 	DataIterator itrI = dataStoreOfI.begin();
-	while(itrU!=this->dataStore.end() && itrI!=dataStoreOfI.end()) {
-		itrU->second.updateIfLargerThan(itrI->second);
+	while(itrU!=dataStore.end() && itrI!=dataStoreOfI.end()) {
+		(itrU->second)->updateIfLargerThan(*(itrI->second));
 		itrU++; itrI++;
 	}
 }
 
 void DataInstance::maximizeWRT(DataInstance& I) {
 	DataMap dataStoreOfI = I.getDataStore();
-	DataIterator itrU = this->dataStore.begin();
+	DataIterator itrU = dataStore.begin();
 	DataIterator itrI = dataStoreOfI.begin();
-	while(itrU!=this->dataStore.end() && itrI!=dataStoreOfI.end()) {
-		itrU->second.updateIfSamllerThan(itrI->second);
+	while(itrU!=dataStore.end() && itrI!=dataStoreOfI.end()) {
+		(itrU->second)->updateIfSamllerThan(*(itrI->second));
 		itrU++; itrI++;
+	}
+}
+
+void DataInstance::minimizeDS() {
+	DataIterator itr = dataStore.begin();
+	while(itr!=dataStore.end()) {
+		(itr->second)->minimize();
+		itr++;
 	}
 }
