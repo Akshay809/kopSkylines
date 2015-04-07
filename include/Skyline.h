7fd795591827f8afd9afcc1e2130f277b8e5e66f
Akshay Agrawal <akshay809@gmail.com>
2015-04-07 16:17:10 +0530

    Update Skyline interface
    Neccessary changes included
    - Define an overloaded function FindSkyline for finding any/all of full, k, p, or kop skylines
    - Declare Skyline finding algorithms prototype, will define in corresponding file in lib
    - Need an efficient way to choose from multiple algorithms for findingSkyline

--- include/Skyline.h
+++ include/Skyline.h
@@ -1,26 +1,47 @@
+#ifndef SKYLIN_H
+#define SKYLIN_H
+
 #include <Data.h>
+#include <DataReader.h>
 #include <Rtree.h>
 #include <Heap.h>
 #include <unordered_set>
 
 using namespace std;
-/*
-struct ObjectInfo {
-	vector<int>& DominatingObjectIDs;
-	double high, low;
-	ObjectInfo(int low, int high, vector<int> &v): low(low), high(high), DominatingObjectIDs(v) {}
-};
-*/
+
+/* full-skyline Algorithms*/
+objectSet& full_(objectSet&);
+
+/* k-Skyline Algorithms */
+objectSet& k_(objectSet&, int);
+
+/* p-Skyline Algorithms */
+objectSet& p_TopDown(objectSet&, double);
+objectSet& p_BottomUp(objectSet&, double);
+
+/* kop-Skyline Algorithms */
+objectSet& kop_(objectSet&, double, int);
+
 
 class Skyline {
+private:
+	objectSet& data, Skyline;
+	string Fname;
 public:
-	Skyline();
-	~Skyline();
-};
+	Skyline(string file_name): Fname(file_name) {
+		objectSet& data = readData(Fname);
+	}
 
-class TopDown : public Skyline {
-public:
-	TopDown();
-	~TopDown();
-	
+	/*TODO: Add preference relation specification*/
+
+	/*Certain Data <=> Single instance instanceSet for every object*/
+	/*Locationally Uncertain Data <=> Multi-instance instanceSet for atleast one object*/
+	/*Existentially Uncertain Data <=> TODO: need to modfiy current weight allocation schema*/
+
+	virtual void findSkyline() {}				//full-Skyline
+	virtual void findSkyline(int k) {}			//k-Skyline
+	virtual void findSkyline(double p) {}		//p-Skyline
+	virtual void findSkyline(double p, int k) {}	//kop-Skyline
+
+	~Skyline();	
 };
