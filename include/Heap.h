22b745eaeb88d7cea3ff643083c2eb522940c35c
Akshay Agrawal <akshay809@gmail.com>
2015-05-03 15:52:01 +0530

    Heap implementation complete

--- include/Heap.h
+++ include/Heap.h
@@ -1,17 +1,54 @@
-#ifndef HEAP_H
-#define HEAP_H
+#ifndef CUSTOM_HEAP_H
+#define CUSTOM_HEAP_H
 
 #include <Data.h>
+#include <algorithm>
+
 using namespace std;
 
 class Heap {
+private:
+	vector<DataInstance>& instances;
 public:
-	Heap(instanceSet instances);
+	struct lower;
+	Heap(vector<DataInstance>&);
 	bool isEmpty();
 	bool isNotEmpty() {	return !isEmpty(); }
 	DataInstance& top();
 	void pop();
-	~Heap();
+	void push(DataInstance&);
+	~Heap() {}
 };
+
+struct Heap::lower {
+	int x;
+	lower(int x): x(x) {}
+	bool operator()(const DataInstance& i1, const DataInstance& i2) {
+		return (i1.getKey())>(i2.getKey());
+	}
+};
+
+Heap::Heap(vector<DataInstance>& instances): instances(instances) {
+	cout << "Initializing the Heap ...." << endl;
+	std::make_heap(instances.begin(), instances.end(), lower(0));
+}
+
+bool Heap::isEmpty() {
+	return instances.size()==0;
+}
+
+DataInstance& Heap::top() {
+	instances.front();
+}
+
+void Heap::pop() {
+	std::pop_heap(instances.begin(), instances.end(), lower(0));
+	instances.pop_back();
+}
+
+void Heap::push(DataInstance& newInstance) {
+	instances.push_back(newInstance);
+	std::push_heap(instances.begin(), instances.end(), lower(0));
+}
 
 #endif
--- test/test_prog.cpp
+++ test/test_prog.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <DataReader.h>
 #include <kdTree.h>
+#include <Heap.h>
 
 using namespace std;
 
@@ -15,25 +16,41 @@ int main(int argc, char const *argv[])
 	vector<DataObject*>& DataObjects = readData(Fname);
 	cout << "Number of objects read: " << DataObjects.size() << endl;
 	for (vector<DataObject*>::iterator itr = DataObjects.begin(); itr != DataObjects.end(); ++itr) {
-//		(*itr)->printDataObject();
+		// (*itr)->printDataObject();
+		/*pushing copies, not originals*/
 		Umins.push_back((*itr)->Umin);
 	}
+
+
 	kdTree T(Umins);
 	// T.printTree();
 	DataInstance le(DataObject::Origin);
 	DataInstance ue(DataObject::Origin);
 	// le = DataInstance::Origin;
 	vector<double> v1 = {0.0, 0.0};
-	vector<double> v2 = {28.0, 5};
+	vector<double> v2 = {16.0, 14};
 	le.updateDS(v1);
 	ue.updateDS(v2);
 	// le.printDataInstance();
 	// ue.printDataInstance();
 	Rectangle R(le, ue);
-	unordered_set<DataObject*>* ans = T.searchR(R);
+	vector<int>& ans = T.searchR(R);
+
 	// cout << " Answer: " << endl;
-	// for (unordered_set<DataObject*>::iterator itr = ans->begin(); itr != ans->end(); ++itr) {
-	// 	(*itr)->printDataObject();
+	// for (int i = 0; i < ans.size(); ++i) {
+	// 	Umins[ans[i]].printDataInstance();
+	// }
+
+	cout << "Heap top...." << endl;
+	Heap H(Umins);
+
+	// if(H.isNotEmpty()) {
+	// 	DataInstance& I = H.top();
+	// 	I.printDataInstance();
+	// 	H.pop();
+	// 	I = H.top();
+	// 	I.printDataInstance();
+	// 	H.push(ue);
 	// }
 
 	return 0;
